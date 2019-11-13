b478885d8f89e28d1cada1da7c7bbf0e4a85b8f7
Aven <hchen18@ebay.com>
2019-11-13 14:39:23 +0800

    add custom shape LineEllipse

---
+++ javascript/src/js/shape/mxLineEllipse.js
@@ -0,0 +1,23 @@
+function LineEllipseShape() {
+    mxEllipse.call(this);
+};
+mxUtils.extend(LineEllipseShape, mxEllipse);
+LineEllipseShape.prototype.paintVertexShape = function (c, x, y, w, h) {
+    mxEllipse.prototype.paintVertexShape.apply(this, arguments);
+
+    c.setShadow(false);
+    c.begin();
+
+    if (mxUtils.getValue(this.style, 'line') == 'vertical') {
+        c.moveTo(x + w / 2, y);
+        c.lineTo(x + w / 2, y + h);
+    } else {
+        c.moveTo(x, y + h / 2);
+        c.lineTo(x + w, y + h / 2);
+    }
+
+    c.end();
+    c.stroke();
+};
+
+mxCellRenderer.registerShape('lineEllipse', LineEllipseShape);
